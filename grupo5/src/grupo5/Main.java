e640dd81f6217135920767273659f9e1914af97d
Blanca <Blanca@192.168.40.8>
2017-04-04 13:01:41 +0200

    2.0

--- grupo5/src/grupo5/Main.java
+++ grupo5/src/grupo5/Main.java
@@ -3,19 +3,14 @@ package grupo5;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("hola");
-<<<<<<< HEAD
-		
-		
-
-=======
-		System.out.println("grupo5");
 		
 		// Blanca
->>>>>>> branch 'master' of https://github.com/emiberfar/grupo5
-	
-	//Cambio Alvaro
-			System.out.println("hola- adiós");
+		System.out.println("hola");
+		
+		System.out.println("grupo5");
+		
+		//Cambio Alvaro
+		System.out.println("adiÃ³s");
 	}
 
 }
