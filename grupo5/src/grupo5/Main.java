14381d5fb7c3ae9b1ee836ac31abba4fc12179a6
EBF10 <EBF10@DESKTOP-O67OBUT>
2017-04-04 13:10:43 +0200

    Emilio

--- grupo5/src/grupo5/Main.java
+++ grupo5/src/grupo5/Main.java
@@ -10,26 +10,23 @@ public class Main {
 <<<<<<< HEAD
 		
 		// Blanca
-<<<<<<< HEAD
->>>>>>> branch 'master' of https://github.com/emiberfar/grupo5
 	
 	//Cambio Alvaro
 			System.out.println("hola- nos vemos");
-=======
-=======
+
 		//Cambio Alvaro
 		System.out.println("adiÃ³s");
->>>>>>> branch 'master' of https://github.com/emiberfar/grupo5
 
-<<<<<<< HEAD
 	//Cambio Alvaro
 			System.out.println("hola- adiós");
->>>>>>> refs/remotes/origin/master
-=======
+
 		//Cambio Alvaro
 		System.out.println("hola- adiï¿½s");
+		
+		//Cambio Emilio
+		
+		System.out.println("emilio");
 
->>>>>>> branch 'master' of https://github.com/emiberfar/grupo5
 	}
 
 }
