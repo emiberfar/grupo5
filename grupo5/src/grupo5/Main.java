4398fe1dfb29323f0c37ffa04110d6118f5fd333
EBF10 <EBF10@DESKTOP-O67OBUT>
2017-04-04 13:01:51 +0200

    Signed-off-by: EBF10 <EBF10@DESKTOP-O67OBUT>

--- grupo5/src/grupo5/Main.java
+++ grupo5/src/grupo5/Main.java
@@ -7,13 +7,7 @@ public class Main {
 		
 		
 		// Blanca
-<<<<<<< HEAD
-		System.out.println("grupo5");
-		
-=======
->>>>>>> branch 'master' of https://github.com/emiberfar/grupo5
-	
->>>>>>> branch 'master' of https://github.com/emiberfar/grupo5
+
 	//Cambio Alvaro
 			System.out.println("hola- adiós");
 	}
