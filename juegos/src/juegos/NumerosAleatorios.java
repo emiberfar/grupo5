363fe31ce7831d86a3f5b89836d201185199ed28
Blanca <Blanca@192.168.40.8>
2017-04-25 12:09:32 +0200

    Documentacion de  Numeros aleatorios (Blanca)

--- juegos/src/juegos/NumerosAleatorios.java
+++ juegos/src/juegos/NumerosAleatorios.java
@@ -7,22 +7,31 @@ public class numerosaleatorios {
 
 	public static void jugar() {
 
-		// Juego adivinar numero.
-
+		/* <h2> Juego adivinar numero </h2>
+		 * Esta función escanea, o mejor dicho, registra el número que introduce el usuario
+		 */
 		Scanner input = new Scanner(System.in);
 
+		// Crea un número random que <b>no supere de 100</b>, para luego adivinarlo
 		int numeroDado, random;
 		random = (int) (Math.random() * 100) + 1;
 
+		/* 
+		 * Mensaje de bienvenida, da la descripción del juego al usuario
+		 */
 		System.out.println("¡Bienvenido! Este es el juego de números aleatorios.");
-		System.out.println(
-				"Consiste en adivinar el número, de 1 al 100. La máquina te dirá si el número que has introducido es mayor o menor.");
+		System.out.println("Consiste en adivinar el número, de 1 al 100. La máquina te dirá si el número que has introducido es mayor o menor.");
 
 		System.out.println(" ");
 		System.out.println("---------------------");
 		System.out.println("Introduce un número: ");
 		numeroDado = input.nextInt();
 
+		/*
+		 * <b>Si el número dado es igual al número random entonces ha acertado</b>
+		 * En el <b>caso contrario</b>, sigue intentándolo. El programa <b>dará pistas</b> si es mayor o número el número que 
+		 * ha introducido comparado con el numero random <b>(numeroDado == random)</b>
+		 */
 		for (int i = 0; i < 100; i++) {
 			{
 				if (numeroDado == random) {
@@ -49,7 +58,11 @@ public class numerosaleatorios {
 			}
 		}
 
-		// Fin juego adviniar numero > Blanca
+		/* Fin juego adviniar numero > Blanca
+		 * @author @Blanca
+		 * @throws SerletException
+		 * @throws IOException
+		 */
 
 	}
 
